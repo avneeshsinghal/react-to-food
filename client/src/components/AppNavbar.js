37137b68c40c4b33b889387900ddc8e42bbbaea5
avneeshsinghal <avneesh1995@gmail.com>
2018-12-27 17:05:36 +0530

    Mend git

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -30,7 +30,7 @@ class AppNavbar extends Component{
                 <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                 <NavItem>
-                    <NavLink href="https://github.com/bradtraversy">Github</NavLink>
+                    <NavLink href="https://github.com/avneeshsinghal">Github</NavLink>
 
                 </NavItem>
                 </Nav>
